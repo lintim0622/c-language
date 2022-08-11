f15ad37c83ec6082e66944acf5b8d071ae7accf1
lintim0622 <lintim0622@smail.nchu.edu.tw>
2022-08-11 12:04:35 +0800

    2022/08/10

---
+++ ch3/prog3_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 12400;
+    double num2 = 5.234;
+    printf("%d is an integer\n", num);
+    printf("%f is a double\n", num2);
+    return 0;
+}
